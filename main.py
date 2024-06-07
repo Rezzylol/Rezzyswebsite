9ce2f58f9575e96118c44d9fe3ede983edf1f9a3
Olioli <101850992+Rezzylol@users.noreply.github.com>
2024-06-07 20:43:38 +1000

    looool
    added some cool ascii art
    made some buttons useful n shit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import request
 import logging
 from flask import Flask, render_template
 # from gunicorn.app.base import BaseApplication
@@ -8,10 +9,16 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__, static_folder='static')
 
+@app.before_request
+def before_request():
+    request.user_ip = request.remote_addr
+
 @app.route("/")
 def home_route():
-    return render_template("home.html")
-
+    return render_template("home.html", user_ip=request.user_ip)
+@app.route("/ip")
+def show_ip():
+    return f"Your IP address is: {request.user_ip}"  # This route is no longer used
 # class StandaloneApplication(BaseApplication):
 #     def __init__(self, app, options=None):
 #         self.application = app
@@ -26,6 +33,13 @@ def home_route():
 # 
 #     def load(self):
 #         return self.application
+
+import random
+
+@app.context_processor
+def inject_visitor_number():
+    visitor_number = random.randint(2000, 50000)
+    return dict(visitor_number=visitor_number)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
