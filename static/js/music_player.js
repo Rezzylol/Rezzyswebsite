b224857a52a0c8fd903b10378823fd8361e2626b
Olioli <101850992+Rezzylol@users.noreply.github.com>
2024-06-08 22:23:40 +1000

    maybe?

--- static/js/music_player.js
+++ static/js/music_player.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const audio = new Audio('{{ url_for("static", filename="music/bn2meloop.mp3") }}');
+window.addEventListener('load', () => {
+    const audio = new Audio('https://rezzy.fr/static/music/bn2meloop.mp3');
     audio.loop = true;
     audio.play();
 });
