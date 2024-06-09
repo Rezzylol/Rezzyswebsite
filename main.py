a7f9ee938a281721d185ce03ece4cdea76c7f2fa
Olioli <101850992+Rezzylol@users.noreply.github.com>
2024-06-10 05:39:15 +1000

    changed everything made basic music player

--- main.py
+++ main.py
@@ -28,22 +28,9 @@ def about_route():
 def silly_route():
     return render_template("silly.html", user_ip=request.user_ip)
 
-@app.route("/yip_box", methods=["GET", "POST"])
-def shout_box_route():
-    if request.method == "POST":
-        username = request.form.get("username")
-        message = request.form.get("message")
-        if username and message:
-            key_value_storage("store", "yip_box", username, message)
-    messages = key_value_storage("retrieve", "yip_box", "", "")
-<<<<<<< HEAD
-    if messages.get("upstream_service_result_code") != 200:
-        messages = {"kv_pairs": []}
-    return render_template("yip_box.html", user_ip=request.user_ip, messages=messages.get("kv_pairs", []))
-=======
-    return render_template("shout_box.html", user_ip=request.user_ip, messages=messages.get("kv_pairs", []))
->>>>>>> parent of 599b881 (fixed?)
-
+@app.route("/yip_box")
+def yip_box_route():
+    return render_template("yip_box.html", user_ip=request.user_ip)
 
 import random
 
--- static/js/music_player.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-    const audio = new Audio('https://rezzy.fr/static/music/bn2meloop.mp3');
-    audio.loop = true;
-    audio.play();
-});
-
-
-
