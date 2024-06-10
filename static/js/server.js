82487845c338d5c59af56a2e2fb60a8616d0a84a
Olioli <101850992+Rezzylol@users.noreply.github.com>
2024-06-10 20:59:22 +1000

    implemented yip_box
    i know damn well this wont work but

---
+++ static/js/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve the yip_box.html file from the /templates directory
+app.get('/yip_box', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../templates/yip_box.html'));
+});
+
+// Serve static files from the /static directory
+app.use('/static', express.static(path.join(__dirname, '../')));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
