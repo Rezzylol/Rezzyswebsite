7980f0d7effc3909fedb237d4b0dfe980584f49a
Olioli <101850992+Rezzylol@users.noreply.github.com>
2024-06-10 21:05:50 +1000

    update before i roll back if it dont work

--- static/js/server.js
+++ static/js/server.js
@@ -18,14 +18,17 @@ app.use('/static', express.static(path.join(__dirname, '../')));
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('chat message', (data) => {
-        io.emit('chat message', data);
+        console.log(`Received message: ${data.message}`);
+        io.emit('chat message', data); // Emit the message to all clients
     });
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
 
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
