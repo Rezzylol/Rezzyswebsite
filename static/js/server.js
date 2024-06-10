826aa45f223e4ee056501246b001d95b80cd3065
Olioli <101850992+Rezzylol@users.noreply.github.com>
2024-06-10 21:13:32 +1000

    Revert "update before i roll back if it dont work"
    This reverts commit 7980f0d7effc3909fedb237d4b0dfe980584f49a.

--- static/js/server.js
+++ static/js/server.js
@@ -18,17 +18,14 @@ app.use('/static', express.static(path.join(__dirname, '../')));
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('chat message', (data) => {
-        console.log(`Received message: ${data.message}`);
-        io.emit('chat message', data); // Emit the message to all clients
+        io.emit('chat message', data);
     });
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
 
-
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
